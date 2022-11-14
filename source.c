1f73ddeff863753044de4577ef29a890f39efbb2
theodoro mimura <theomeme21@gmail.com>
2022-11-14 19:06:36 -0300

    Create the studant structure

---
+++ source.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define max 60
+
+struct studant
+{
+    char name[max];
+    int age;
+    float weight;
+    float height;
+};
+typedef strunct studant studant;
+
+studant get_studant()
+{
+    return studant;
+}
+
+float high_height(studant.height)
+{
+    return studant.height;
+
+}
+
+
+int main(void)
+{
+
+}
